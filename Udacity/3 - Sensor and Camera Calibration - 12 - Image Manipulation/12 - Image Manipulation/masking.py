68f077dd0fb4c5fc30b58de8ab03dc402c0d49a7
alexheijne1980 <alex_heijnen@hotmail.com>
2021-11-22 10:09:44 -0800

    Update November 22nd

--- Udacity/3 - Sensor and Camera Calibration - 12 - Image Manipulation/12 - Image Manipulation/masking.py
+++ Udacity/3 - Sensor and Camera Calibration - 12 - Image Manipulation/12 - Image Manipulation/masking.py
@@ -12,18 +12,15 @@ def create_mask(path, color_threshold):
     - img [array]: RGB image array
     - mask [array]: binary array
     """
-    # Too slow
     img = Image.open(path)
-    width, height = img.size
-    print(width, height)
-    mask = np.zeros((height, width))
-    for column in range(width):
-        for row in range(height):
-            RGB = img.getpixel((column, row))
-            if RGB[0] < color_threshold[0] or RGB[1] < color_threshold[1] or RGB[2] < color_threshold[2]:
-                mask[row][column] = 0
-            else:
-                mask[row][column] = 255                          
+    img = img.convert('RGB')
+    img = np.array(img)
+    R = img[..., 0] 
+    G = img[..., 1]
+    B = img[..., 2]
+    rt, gt, bt = color_threshold
+    # Generate a numpy array with boolean values.
+    mask = (R > rt) & (G > gt) & (B > bt)                 
     
     return img, mask
 
@@ -34,23 +31,14 @@ def mask_and_display(img, mask):
     - img [array]: HxWxC image array
     - mask [array]: HxW mask array
     """
-       
-    mask_image = Image.fromarray(mask)
-    masked_image2 = mask_image.copy()
-    masked_image2.convert("RGB")
-    masked_image2 = img * masked_image2
-    img2 = img.copy()
-    #img2.convert("RGBA")
-    #img2.putalpha(mask_image2)
-       
+    # Combine the axis with RGB values and the boolean values in the mask.
+    masked_image = img * np.stack([mask] * 3, axis = 2)
     fig, (ax1, ax2, ax3) = plt.subplots(1,3)
     ax1.imshow(img)
-    ax2.imshow(mask_image) 
-    ax3.imshow(masked_image2)
-    plt.show()
-        
-    return
-
+    ax2.imshow(mask) 
+    ax3.imshow(masked_image)
+    plt.show()  
+    
 if __name__ == '__main__':
     path = 'data/images/segment-1231623110026745648_480_000_500_000_with_camera_labels_38.png'
     color_threshold = [128, 128, 128]
--- Udacity/3 - Sensor and Camera Calibration - 12 - Image Manipulation/12 - Image Manipulation/statistics.py
+++ Udacity/3 - Sensor and Camera Calibration - 12 - Image Manipulation/12 - Image Manipulation/statistics.py
@@ -1,3 +1,11 @@
+import glob
+import matplotlib.pyplot as plt
+import numpy as np
+# import seaborn as sn
+from PIL import Image
+from PIL import ImageStat
+
+
 def calculate_mean_std(image_list):
     """
     calculate mean and std of image list
@@ -7,8 +15,19 @@ def calculate_mean_std(image_list):
     - mean [array]: 1x3 array of float, channel wise mean
     - std [array]: 1x3 array of float, channel wise std
     """
-    # IMPLEMENT THIS FUNCTION
-    return mean, std
+    means = []
+    stds = []
+    for path in image_list:
+        img = Image.open(path)
+        img = img.convert('RGB')
+        stat = ImageStat.Stat(img)
+        means.append(np.array(stat.mean))
+        stds.append(np.array(stat.var)**0.5)
+    
+    total_mean = np.mean(means, axis = 0) 
+    total_std = np.mean(stds, axis = 0)
+    
+    return total_mean, total_std
 
 
 def channel_histogram(image_list):
@@ -17,9 +36,26 @@ def channel_histogram(image_list):
     args:
     - image_list [list[str]]: list of image paths
     """
-    # IMPLEMENT THIS FUNCTION
-
-
+    red = []
+    green = []
+    blue = []  
+    for path in image_list:
+        img = Image.open(path)
+        img = img.convert('RGB')
+        img = np.array(img)
+        R = img[..., 0] 
+        G = img[..., 1]
+        B = img[..., 2]
+        red.extend(R.flatten().tolist())
+        blue.extend(B.flatten().tolist())
+        green.extend(G.flatten().tolist())
+    # Plot histograms using matplotlib as seaborn is not yet installed.
+    fig, (ax1, ax2, ax3) = plt.subplots(1,3)
+    ax1.hist(red, color = 'red')
+    ax2.hist(green, color = 'green') 
+    ax3.hist(blue, color = 'blue')
+    plt.show()
+                        
 if __name__ == "__main__": 
     image_list = glob.glob('data/images/*')
     mean, std = calculate_mean_std(image_list)
